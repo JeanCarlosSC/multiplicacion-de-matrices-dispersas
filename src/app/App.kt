996c5e23e51a75f630baf0bb4aef7f53871187b7
JeanCarlos911 <jeancarlosodi@gmail.com>
2021-01-20 08:35:21 -0500

    added mainbar basics

--- src/app/App.kt
+++ src/app/App.kt
@@ -1,14 +1,46 @@
 package app
 
+import lib.sRAD.gui.sComponent.SMenuBar
 import lib.sRAD.gui.tool.setMainBar
 import lib.sRAD.gui.tool.setProperties
 import javax.swing.JFrame
+import javax.swing.JMenu
+import javax.swing.JMenuItem
+import javax.swing.JOptionPane
 
 class App: JFrame(){
 
     init {
+        addMenuBar()
+
         setMainBar("Multiplicación de matrices dispersas")
         setProperties()
+    }
+
+    private fun addMenuBar() {
+        val menuBar = SMenuBar()
+        add(menuBar)
+
+        //File
+        val file = JMenu("File")
+        menuBar.add(file)
+
+        val settings = JMenuItem("Settings...")
+        file.add(settings)
+
+        //Help
+        val help = JMenu("Help")
+        menuBar.add(help)
+
+        val about = JMenuItem("About")
+        about.addActionListener {
+            JOptionPane.showMessageDialog(null, "" +
+                    "This software has been developed by Jean Carlos Santoya Cabrera." +
+                    "\nReferences:" +
+                    "\n - https://porcomputador.com/", "About", JOptionPane.INFORMATION_MESSAGE
+            )
+        }
+        help.add(about)
     }
 
 }
