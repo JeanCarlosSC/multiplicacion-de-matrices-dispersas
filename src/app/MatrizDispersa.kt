302f6a0d773f296450a596220df8692adaded366
JeanCarlos911 <jeancarlosodi@gmail.com>
2021-01-19 19:20:46 -0500

    todos lo relacionado con matrices dispersas, implementado

--- src/app/MatrizDispersa.kt
+++ src/app/MatrizDispersa.kt
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane
 
 class MatrizDispersa {
 
-    private val nodosColumna = mutableListOf<NodoColumna>()
+    internal val nodosColumna = mutableListOf<NodoColumna>()
 
     fun insertar(fila: Int, columna: Int, valor: Int) {
         //verifica que la posición sea válida
@@ -45,9 +45,9 @@ class MatrizDispersa {
         val nodo = NodoColumna(columna)
         nodo.insertar(fila, valor)
 
-        //si el valor de la columna es mayor o igual que algún nodo columna
+        //si el valor de la columna es menor que algún nodo columna
         for (i in nodosColumna.indices) {
-            if (columna >= nodosColumna[i].columna) {
+            if (columna < nodosColumna[i].columna) {
                 nodosColumna.add(i, nodo)
 
                 return
@@ -59,25 +59,50 @@ class MatrizDispersa {
     }
 
     private fun eliminar(fila: Int, columna: Int) {
+        //elimina si el nodo existe
         for (i in nodosColumna.indices) {
             if (nodosColumna[i].columna == columna) {
                 nodosColumna[i].eliminar(fila)
+                //si el nodo queda vacío, se retira
                 if (nodosColumna[i].getSize() == 0)
-                    nodosColumna.removeAt(columna)
+                    nodosColumna.removeAt(i)
+                return
             }
         }
     }
 
+    fun obtenerValor(fila: Int, columna: Int): Int {
+        for (i in nodosColumna) {
+            if (i.columna == columna)
+                return i.obtenerValor(fila)
+        }
+        return 0
+    }
+
+    override fun toString(): String {
+        var datos = "Fila      Columna   Valor"
+
+        if (nodosColumna.isEmpty())
+            return "La matriz está vacía"
+
+        for (j in nodosColumna) {
+            for (i in j.nodosFila) {
+                datos += "\n${"%7d   %7d   %7d".format(i.fila, j.columna, i.valor)}"
+            }
+        }
+
+        return datos
+    }
+
 }
 
-class NodoFila (fila: Int) {
-    val fila = fila
-}
+class NodoFila (
+    val fila: Int,
+    var valor: Int
+    )
 
-class NodoColumna(columna: Int) {
-    val columna = columna
-
-    private val nodosFila = mutableListOf<NodoFila>()
+class NodoColumna(val columna: Int) {
+    internal val nodosFila = mutableListOf<NodoFila>()
 
     fun eliminar (fila: Int) {
         for (i in nodosFila.indices) {
@@ -88,12 +113,59 @@ class NodoColumna(columna: Int) {
         }
     }
 
-    fun insertar (fila: Int, columna: Int){
+    fun insertar (fila: Int, valor: Int){
+        //si el nodo existe
+        for (i in nodosFila) {
+            if (i.fila == fila) {
+                i.valor = valor
+                return
+            }
+        }
 
+        //crea nodo
+        val nodo = NodoFila(fila, valor)
+
+        //si es menor que alguno
+        for (i in nodosFila.indices) {
+            if (fila < nodosFila[i].fila) {
+                nodosFila.add(i, nodo)
+                return
+            }
+        }
+
+        //si es el mayor
+        nodosFila.add(nodo)
+    }
+
+    fun obtenerValor (fila: Int): Int {
+        for (i in nodosFila) {
+            if(i.fila == fila) {
+                return i.valor
+            }
+        }
+        return 0
     }
 
     fun getSize (): Int {
         return nodosFila.size
     }
 
+}
+
+fun multiplicar(matrizA: MatrizDispersa, matrizB: MatrizDispersa): MatrizDispersa{
+    val result = MatrizDispersa()
+
+    //apunta a cada valor de la matriz a
+    for (j in matrizA.nodosColumna) {
+        for (i in j.nodosFila) {
+            //apunta a todas las columnas de la matriz b por cada valor de a
+            for (b in matrizB.nodosColumna) {
+                //suma el producto al resultado
+                result.insertar(i.fila, b.columna,
+                    result.obtenerValor(i.fila, b.columna) + i.valor*b.obtenerValor(j.columna))
+            }
+        }
+    }
+
+    return result
 }
